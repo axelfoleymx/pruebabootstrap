ccaae1019e10b675bc754eb124082e108dea5ba3
axelfoleymx <a01164173@gmail.com>
2016-09-22 20:31:35 -0500

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def x
+  end
+
+  def y
+  end
+
+  def z
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/x'
+
+  get 'pages/y'
+
+  get 'pages/z'
+
+  root 'pages#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
